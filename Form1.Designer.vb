23590f0e5f4419fce204d502932e9556756c5709
marwan eldesouki <37198610+marwaneldesouki@users.noreply.github.com>
2020-01-19 01:50:31 +0200

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,168 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.Black
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox1.ForeColor = System.Drawing.Color.Silver
+        Me.TextBox1.Location = New System.Drawing.Point(12, 43)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(236, 24)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = "Put The Link Here.."
+        '
+        'Button1
+        '
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.ForeColor = System.Drawing.Color.Red
+        Me.Button1.Location = New System.Drawing.Point(12, 84)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 29)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Detect"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.ForeColor = System.Drawing.Color.Silver
+        Me.Label1.Location = New System.Drawing.Point(9, 126)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(76, 17)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Status: ....."
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.Black
+        Me.Label2.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Label2.ForeColor = System.Drawing.Color.Red
+        Me.Label2.Location = New System.Drawing.Point(0, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(272, 26)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "I-F Fake Links Detector By Deso"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Black
+        Me.Label3.ForeColor = System.Drawing.Color.Red
+        Me.Label3.Location = New System.Drawing.Point(254, 5)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(16, 17)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "X"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.Black
+        Me.Label4.ForeColor = System.Drawing.Color.Red
+        Me.Label4.Location = New System.Drawing.Point(230, 5)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(18, 17)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "--"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.ForeColor = System.Drawing.Color.Red
+        Me.Label5.Location = New System.Drawing.Point(-26, 14)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(440, 17)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "______________________________________________________"
+        '
+        'Button2
+        '
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.ForeColor = System.Drawing.Color.Red
+        Me.Button2.Location = New System.Drawing.Point(93, 84)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(88, 29)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "Rest"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.ForeColor = System.Drawing.Color.Red
+        Me.Button3.Location = New System.Drawing.Point(187, 84)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(83, 29)
+        Me.Button3.TabIndex = 8
+        Me.Button3.Text = "About"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(272, 151)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label5)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "IG Fake Links Detector By Deso"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,144 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Text.RegularExpressions
+Imports System.Threading
+Public Class Form1
+    Function chk1(str As String)
+
+        If (str.Contains("instagram.com") Or str.Contains("facebook.com")) Then
+            Label1.Text = "Status: Not Fake"
+            Label1.ForeColor = Color.Green
+            MsgBox("This is the official Link")
+        ElseIf (str.Contains("auditcb.cz")) Then
+            Dim httpWebRequest As HttpWebRequest = CType(WebRequest.Create(str), HttpWebRequest)
+            httpWebRequest.Proxy = Nothing
+            Using streamReader As StreamReader = New StreamReader(httpWebRequest.GetResponse().GetResponseStream())
+                Dim prompt As String = streamReader.ReadToEnd()
+
+                rgx1(prompt)
+
+
+            End Using
+        ElseIf (str.Contains("goldwavss")) Then
+            Dim httpWebRequest As HttpWebRequest = CType(WebRequest.Create(str), HttpWebRequest)
+            httpWebRequest.Proxy = Nothing
+            Using streamReader As StreamReader = New StreamReader(httpWebRequest.GetResponse().GetResponseStream())
+                Dim prompt As String = streamReader.ReadToEnd()
+
+                If (prompt.Contains("http://postingerdesign.com/live.php") Or prompt.Contains("action=""http://postingerdesign.com/nolive.php""")) Then
+                    Label1.Text = "Status: Fake"
+                    Label1.ForeColor = Color.Red
+                Else
+                    Label1.Text = "Status: Not Fake"
+                    Label1.ForeColor = Color.Green
+                End If
+            End Using
+        ElseIf (str.Contains("dentaldisinfection.com")) Then
+
+            Dim httpWebRequest As HttpWebRequest = CType(WebRequest.Create(str), HttpWebRequest)
+            httpWebRequest.Proxy = Nothing
+            Using streamReader As StreamReader = New StreamReader(httpWebRequest.GetResponse().GetResponseStream())
+                Dim prompt As String = streamReader.ReadToEnd()
+
+                If (prompt.Contains("name=""user_id_victim""")) Then
+                    Label1.Text = "Status: Fake"
+                    Label1.ForeColor = Color.Red
+                Else
+                    Label1.Text = "Status: Not Fake"
+                    Label1.ForeColor = Color.Green
+                End If
+            End Using
+        Else
+            Label1.Text = "Status: Not Fake"
+            Label1.ForeColor = Color.Green
+        End If
+    End Function
+    Function rgx1(str As String)
+
+
+        Dim r As New Regex("<frame name=""main"" src=""(.*?)"">")
+        Dim matchess As MatchCollection = r.Matches(str)
+
+        For Each itemcode As Match In matchess
+            chk2(itemcode.Groups(1).Value)
+        Next
+
+
+    End Function
+
+    Function chk2(str As String)
+        Dim httpWebRequest As HttpWebRequest = CType(WebRequest.Create(str), HttpWebRequest)
+        httpWebRequest.Proxy = Nothing
+        Using streamReader As StreamReader = New StreamReader(httpWebRequest.GetResponse().GetResponseStream())
+            Dim prompt As String = streamReader.ReadToEnd()
+
+            If (prompt.Contains("id=""pwd""") Or prompt.Contains("id=""email""") Or prompt.Contains("action=""http://postingerdesign.com/live.php""") Or prompt.Contains("action=""http://postingerdesign.com/nolive.php""")) Then
+                Label1.Text = "Status: Fake"
+                Label1.ForeColor = Color.Red
+            Else
+                Label1.Text = "Status: Not Fake"
+                Label1.ForeColor = Color.Green
+            End If
+
+
+        End Using
+    End Function
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If (TextBox1.Text = "") Then
+            MsgBox("Plz Fill The Box")
+
+        Else
+            Label1.Text = "Status: Wait> Checking The Link..."
+            Label1.ForeColor = Color.Red
+            Me.Refresh()
+            Thread.Sleep(300)
+            chk1(TextBox1.Text)
+        End If
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+        End
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+        WindowState = FormWindowState.Minimized
+    End Sub
+    Private isMouseDown As Boolean
+    Private mouseOffset As Point
+    Private Sub Label2_MouseDown(sender As Object, e As MouseEventArgs) Handles Label2.MouseDown
+        Dim flag As Boolean = e.Button = MouseButtons.Left
+        If flag Then
+            ' The following expression was wrapped in a checked-expression
+            Me.mouseOffset = New Point(0 - e.X, 0 - e.Y)
+            Me.isMouseDown = True
+        End If
+    End Sub
+
+    Private Sub Label2_MouseMove(sender As Object, e As MouseEventArgs) Handles Label2.MouseMove
+        Dim flag As Boolean = Me.isMouseDown
+        If flag Then
+            Dim mousePosition As Point = Control.MousePosition
+            mousePosition.Offset(Me.mouseOffset.X, Me.mouseOffset.Y)
+            Me.Location = mousePosition
+        End If
+    End Sub
+
+    Private Sub Label2_MouseUp(sender As Object, e As MouseEventArgs) Handles Label2.MouseUp
+        Dim flag As Boolean = e.Button = MouseButtons.Left
+        If flag Then
+            Me.isMouseDown = False
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox1.Clear()
+        Label1.Text = "Status: ....."
+        Label1.ForeColor = Color.Silver
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        MessageBox.Show("Coded by Deso" + vbCr + "IG: Marwaneldesouki" + vbCrLf + "GITHUB: Marwaneldesouki", "About")
+
+    End Sub
+End Class
